79a75e1b4c6b1422038d2439f5037f010f85acb3
Richard Orr <richard@ourresearch.org>
2021-11-19 15:05:18 +0000

    convert all varchars to text, skip headers, skip json null byte removal

--- main.sql
+++ main.sql
@@ -34,16 +34,16 @@ DROP TABLE IF EXISTS mag.affiliations;
 CREATE TABLE mag.affiliations(
     AffiliationId BIGINT PRIMARY KEY,
     Rank INT,
-    NormalizedName VARCHAR(150),
+    NormalizedName TEXT,
     DisplayName TEXT,
-    GridId VARCHAR(15),
+    GridId TEXT,
     RorId TEXT,
     OfficialPage TEXT,
     WikiPage TEXT,
     PaperCount BIGINT,
     PaperFamilyCount BIGINT,
     CitationCount BIGINT,
-    Iso3166Code VARCHAR(3),
+    Iso3166Code TEXT,
     Latitude FLOAT8,
     Longitude FLOAT8,
     CreatedDate DATE,
@@ -51,7 +51,7 @@ CREATE TABLE mag.affiliations(
     geom geometry(POINT,4326)
   );
 
-\COPY mag.affiliations(AffiliationId, Rank, NormalizedName, DisplayName, GridId, RorId, OfficialPage, WikiPage, PaperCount, PaperFamilyCount, CitationCount, Iso3166Code, Latitude, Longitude, CreatedDate, UpdatedDate) FROM '../input/export/mag/Affiliations.txt' null as '';
+\COPY mag.affiliations(AffiliationId, Rank, NormalizedName, DisplayName, GridId, RorId, OfficialPage, WikiPage, PaperCount, PaperFamilyCount, CitationCount, Iso3166Code, Latitude, Longitude, CreatedDate, UpdatedDate) FROM PROGRAM 'tail -n+2 input/mag/Affiliations.txt' null as '';
 
 CREATE INDEX idx_affiliations_NormalizedName ON mag.affiliations(NormalizedName);
 CREATE INDEX idx_affiliations_GridId ON mag.affiliations(GridId);
@@ -70,10 +70,10 @@ CREATE TABLE mag.authorextendedattributes(
     id SERIAL PRIMARY KEY,
     AuthorId BIGINT,
     AttributeType SMALLINT,
-    AttributeValue VARCHAR(100)
+    AttributeValue TEXT
   );
 
-\COPY mag.authorextendedattributes(AuthorId, AttributeType, AttributeValue) FROM '../input/export/mag/AuthorExtendedAttributes.txt' null as '';
+\COPY mag.authorextendedattributes(AuthorId, AttributeType, AttributeValue) FROM PROGRAM 'tail -n+2 input/mag/AuthorExtendedAttributes.txt' null as '';
 
 CREATE INDEX idx_authorextendedattributes_AuthorId ON mag.authorextendedattributes(AuthorId);
 CREATE INDEX idx_authorextendedattributes_AttributeValue ON mag.authorextendedattributes(AttributeValue);
@@ -85,8 +85,8 @@ DROP TABLE IF EXISTS mag.Authors;
 CREATE TABLE mag.Authors(
     AuthorId BIGINT PRIMARY KEY,
     Rank INT,
-    NormalizedName VARCHAR(200),
-    DisplayName VARCHAR(400),
+    NormalizedName TEXT,
+    DisplayName TEXT,
     Orcid TEXT,
     LastKnownAffiliationId BIGINT,
     PaperCount BIGINT,
@@ -96,7 +96,7 @@ CREATE TABLE mag.Authors(
     UpdatedDate timestamp without time zone
   );
 
-\COPY mag.Authors(AuthorId, Rank, NormalizedName, DisplayName, Orcid, LastKnownAffiliationId, PaperCount, PaperFamilyCount, CitationCount, CreatedDate, UpdatedDate) FROM '../input/export/mag/Authors.txt' null as '';
+\COPY mag.Authors(AuthorId, Rank, NormalizedName, DisplayName, Orcid, LastKnownAffiliationId, PaperCount, PaperFamilyCount, CitationCount, CreatedDate, UpdatedDate) FROM PROGRAM 'tail -n+2 input/mag/Authors.txt' null as '';
 
 CREATE INDEX idx_Authors_NormalizedName ON mag.Authors(NormalizedName);
 CREATE INDEX idx_Authors_LastKnownAffiliationId ON mag.Authors(LastKnownAffiliationId);
@@ -108,10 +108,10 @@ CREATE INDEX idx_Authors_LastKnownAffiliationId ON mag.Authors(LastKnownAffiliat
 DROP TABLE IF EXISTS mag.ConferenceInstances;
 CREATE TABLE mag.ConferenceInstances(
     ConferenceInstanceId BIGINT PRIMARY KEY,
-    NormalizedName VARCHAR(50),
-    DisplayName VARCHAR(50),
+    NormalizedName TEXT,
+    DisplayName TEXT,
     ConferenceSeriesId BIGINT,
-    Location VARCHAR(255),
+    Location TEXT,
     OfficialUrl TEXT,
     StartDate DATE,
     EndDate DATE,
@@ -128,7 +128,7 @@ CREATE TABLE mag.ConferenceInstances(
     geom geometry(POINT,4326)
   );
 
-\COPY mag.ConferenceInstances(ConferenceInstanceId, NormalizedName, DisplayName, ConferenceSeriesId, Location, OfficialUrl, StartDate, EndDate, AbstractRegistrationDate, SubmissionDeadlineDate, NotificationDueDate, FinalVersionDueDate, PaperCount, PaperFamilyCount, CitationCount, Latitude, Longitude, CreatedDate) FROM '../input/export/mag/ConferenceInstances.txt' null as '';
+\COPY mag.ConferenceInstances(ConferenceInstanceId, NormalizedName, DisplayName, ConferenceSeriesId, Location, OfficialUrl, StartDate, EndDate, AbstractRegistrationDate, SubmissionDeadlineDate, NotificationDueDate, FinalVersionDueDate, PaperCount, PaperFamilyCount, CitationCount, Latitude, Longitude, CreatedDate) FROM PROGRAM 'tail -n+2 input/mag/ConferenceInstances.txt' null as '';
 
 CREATE INDEX idx_ConferenceInstances_NormalizedName ON mag.ConferenceInstances(NormalizedName);
 CREATE INDEX idx_ConferenceInstances_ConferenceSeriesId ON mag.ConferenceInstances(ConferenceSeriesId);
@@ -145,15 +145,15 @@ DROP TABLE IF EXISTS mag.ConferenceSeries;
 CREATE TABLE mag.ConferenceSeries(
     ConferenceSeriesId BIGINT PRIMARY KEY,
     Rank INT,
-    NormalizedName VARCHAR(50),
-    DisplayName VARCHAR(200),
+    NormalizedName TEXT,
+    DisplayName TEXT,
     PaperCount BIGINT,
     PaperFamilyCount BIGINT,
     CitationCount BIGINT,
     CreatedDate DATE
   );
 
-\COPY mag.ConferenceSeries(ConferenceSeriesId, Rank, NormalizedName, DisplayName, PaperCount, PaperFamilyCount, CitationCount, CreatedDate) FROM '../input/export/mag/ConferenceSeries.txt' null as '';
+\COPY mag.ConferenceSeries(ConferenceSeriesId, Rank, NormalizedName, DisplayName, PaperCount, PaperFamilyCount, CitationCount, CreatedDate) FROM PROGRAM 'tail -n+2 input/mag/ConferenceSeries.txt' null as '';
 
 CREATE INDEX idx_ConferenceSeries_NormalizedName ON mag.ConferenceSeries(NormalizedName);
 
@@ -164,9 +164,9 @@ DROP TABLE IF EXISTS mag.Journals;
 CREATE TABLE mag.Journals(
     JournalId BIGINT  PRIMARY KEY,
     Rank INT,
-    NormalizedName VARCHAR(255),
-    DisplayName VARCHAR(255),
-    Issn VARCHAR(15),
+    NormalizedName TEXT,
+    DisplayName TEXT,
+    Issn TEXT,
     Issns TEXT,
     IsOa BOOLEAN,
     IsInDoaj BOOLEAN,
@@ -179,7 +179,7 @@ CREATE TABLE mag.Journals(
     UpdatedDate timestamp without time zone
   );
 
-\COPY mag.Journals(JournalId, Rank, NormalizedName, DisplayName, Issn, Issns, IsOa, IsInDoaj, Publisher, Webpage, PaperCount, PaperFamilyCount, CitationCount, CreatedDate, UpdatedDate) FROM '../input/export/mag/Journals.txt' null as '';
+\COPY mag.Journals(JournalId, Rank, NormalizedName, DisplayName, Issn, Issns, IsOa, IsInDoaj, Publisher, Webpage, PaperCount, PaperFamilyCount, CitationCount, CreatedDate, UpdatedDate) FROM PROGRAM 'tail -n+2 input/mag/Journals.txt' null as '';
 
 CREATE INDEX idx_Journals_NormalizedName ON mag.Journals(NormalizedName);
 CREATE INDEX idx_Journals_Issn ON mag.Journals(Issn);
@@ -198,9 +198,7 @@ CREATE TABLE mag.PaperAuthorAffiliations(
     OriginalAffiliation TEXT
   );
 
-\! tr -d '\000' < PaperAuthorAffiliations.txt > PaperAuthorAffiliations_.txt
-
-\COPY mag.PaperAuthorAffiliations(PaperId, AuthorId, AffiliationId, AuthorSequenceNumber, OriginalAuthor, OriginalAffiliation)  FROM '../input/export/mag/PaperAuthorAffiliations_.txt' NULL as '';
+\COPY mag.PaperAuthorAffiliations(PaperId, AuthorId, AffiliationId, AuthorSequenceNumber, OriginalAuthor, OriginalAffiliation) FROM PROGRAM 'tail -n+2 input/mag/PaperAuthorAffiliations.txt' NULL as '';
 
 CREATE INDEX idx_PaperAuthorAffiliations_PaperId ON mag.PaperAuthorAffiliations(PaperId);
 CREATE INDEX idx_PaperAuthorAffiliations_AuthorId ON mag.PaperAuthorAffiliations(AuthorId);
@@ -216,7 +214,7 @@ CREATE TABLE mag.PaperReferences(
     PaperReferenceId BIGINT
   );
 
-\COPY mag.PaperReferences(PaperId, PaperReferenceId) FROM '../input/export/mag/PaperReferences.txt' null as '';
+\COPY mag.PaperReferences(PaperId, PaperReferenceId) FROM PROGRAM 'tail -n+2 input/mag/PaperReferences.txt' null as '';
 
 CREATE INDEX idx_PaperReferences_PaperId ON mag.PaperReferences(PaperId);
 CREATE INDEX idx_PaperReferences_PaperReferenceId ON mag.PaperReferences(PaperReferenceId);
@@ -234,7 +232,7 @@ CREATE TABLE mag.PaperResources(
     RelationshipType SMALLINT
   );
 
-\COPY mag.PaperResources(PaperId, ResourceType, ResourceUrl, SourceUrl, RelationshipType) FROM '../input/export/mag/PaperResources.txt' null as '';
+\COPY mag.PaperResources(PaperId, ResourceType, ResourceUrl, SourceUrl, RelationshipType) FROM PROGRAM 'tail -n+2 input/mag/PaperResources.txt' null as '';
 
 CREATE INDEX idx_PaperResources_PaperId ON mag.PaperResources(PaperId);
 
@@ -245,15 +243,15 @@ DROP TABLE IF EXISTS mag.Papers;
 CREATE TABLE mag.Papers(
     PaperId BIGINT PRIMARY KEY,
     Rank INT,
-    Doi VARCHAR(255),
-    DocType VARCHAR(20),
+    Doi TEXT,
+    DocType TEXT,
     Genre TEXT,
     IsParatext BOOLEAN,
     PaperTitle TEXT,
     OriginalTitle TEXT,
     BookTitle TEXT,
     Year SMALLINT,
-    Date TEXT,
+    Date DATE,
     OnlineDate TEXT,
     Publisher TEXT,
     JournalId BIGINT,
@@ -275,13 +273,13 @@ CREATE TABLE mag.Papers(
     BestFreeUrl TEXT,
     BestFreeVersion TEXT,
     DoiLower TEXT,
-    CreatedDate TEXT,
+    CreatedDate DATE,
     UpdatedDate timestamp without time zone
   );
 
-\! tr -d '\000' < Papers.txt > Papers_.txt
+\! tr -d '\000' < input/mag/Papers.txt > input/mag/Papers_.txt
 
-\COPY mag.Papers(PaperId, Rank, Doi, DocType, Genre, IsParatext, PaperTitle, OriginalTitle, BookTitle, Year, Date, OnlineDate, Publisher, JournalId, ConferenceSeriesId, ConferenceInstanceId, Volume, Issue, FirstPage, LastPage, ReferenceCount, CitationCount, EstimatedCitation, OriginalVenue, FamilyId, FamilyRank, DocSubTypes, OaStatus, BestUrl, BestFreeUrl, BestFreeVersion, DoiLower, CreatedDate, UpdatedDate) FROM '../input/export/mag/Papers_.txt' NULL as '';
+\COPY mag.Papers(PaperId, Rank, Doi, DocType, Genre, IsParatext, PaperTitle, OriginalTitle, BookTitle, Year, Date, OnlineDate, Publisher, JournalId, ConferenceSeriesId, ConferenceInstanceId, Volume, Issue, FirstPage, LastPage, ReferenceCount, CitationCount, EstimatedCitation, OriginalVenue, FamilyId, FamilyRank, DocSubTypes, OaStatus, BestUrl, BestFreeUrl, BestFreeVersion, DoiLower, CreatedDate, UpdatedDate) FROM PROGRAM 'tail -n+2 input/mag/Papers_.txt' NULL as '';
 
 CREATE INDEX idx_Papers_PaperTitle ON mag.Papers(PaperTitle);
 CREATE INDEX idx_Papers_OriginalTitle ON mag.Papers(OriginalTitle);
@@ -302,7 +300,7 @@ CREATE TABLE mag.PaperExtendedAttributes(
     AttributeType SMALLINT,
     AttributeValue TEXT
   );
-\COPY mag.PaperExtendedAttributes(PaperId, AttributeType, AttributeValue) FROM '../input/export/mag/PaperExtendedAttributes.txt' WITH CSV delimiter E'\t'  ESCAPE '\' QUOTE E'\b'  null as '';
+\COPY mag.PaperExtendedAttributes(PaperId, AttributeType, AttributeValue) FROM 'input/mag/PaperExtendedAttributes.txt' WITH CSV delimiter E'\t'  ESCAPE '\' QUOTE E'\b'  null as '' HEADER;
 -- '
 
 CREATE INDEX idx_PaperExtendedAttributes_PaperId ON mag.PaperExtendedAttributes(PaperId);
@@ -316,7 +314,7 @@ CREATE TABLE mag.PaperUrls(
     PaperId BIGINT,
     SourceType SMALLINT,
     SourceUrl TEXT,
-    LanguageCode VARCHAR(10),
+    LanguageCode TEXT,
     UrlForLandingPage TEXT,
     UrlForPdf TEXT,
     HostType TEXT,
@@ -326,7 +324,7 @@ CREATE TABLE mag.PaperUrls(
     OaiPmhId TEXT
   );
 
-\COPY mag.PaperUrls(PaperId, SourceType, SourceUrl, LanguageCode, UrlForLandingPage, UrlForPdf, HostType, Version, License, RepositoryInstitution, OaiPmhId) FROM '../input/export/mag/PaperUrls.txt' delimiter E'\t' null as '';
+\COPY mag.PaperUrls(PaperId, SourceType, SourceUrl, LanguageCode, UrlForLandingPage, UrlForPdf, HostType, Version, License, RepositoryInstitution, OaiPmhId) FROM PROGRAM 'tail -n+2 input/mag/PaperUrls.txt' delimiter E'\t' null as '';
 
 CREATE INDEX idx_PaperUrls_PaperId ON mag.PaperUrls(PaperId);
 
@@ -346,7 +344,7 @@ CREATE TABLE mag.PaperAbstractsInvertedIndex(
     IndexedAbstract JSONB
   );
 
-\COPY mag.PaperAbstractsInvertedIndex(PaperId, IndexedAbstract) FROM PROGRAM 'cat ../input/export/nlp/PaperAbstractsInvertedIndex.txt*' null as '';
+\COPY mag.PaperAbstractsInvertedIndex(PaperId, IndexedAbstract) FROM PROGRAM 'awk FNR-1 input/nlp/PaperAbstractsInvertedIndex.txt* | sed "s/\\\\\\\\u0000//g"' null as '';
 
 --------------------------------------------------------
 -- PaperCitationContexts
@@ -359,7 +357,7 @@ CREATE TABLE mag.PaperCitationContexts(
     CitationContext TEXT
   );
 
-\COPY mag.PaperCitationContexts(PaperId, PaperReferenceId, CitationContext) FROM '../input/export/nlp/PaperCitationContexts.txt' null as '';
+\COPY mag.PaperCitationContexts(PaperId, PaperReferenceId, CitationContext) FROM PROGRAM 'tail -n+2 input/nlp/PaperCitationContexts.txt' null as '';
 
 CREATE INDEX idx_PaperCitationContexts_PaperId ON mag.PaperCitationContexts(PaperId);
 
@@ -376,14 +374,14 @@ DROP TABLE IF EXISTS mag.EntityRelatedEntities;
 CREATE TABLE mag.EntityRelatedEntities(
     id SERIAL PRIMARY KEY,
     EntityId BIGINT,
-    EntityType VARCHAR(2),
+    EntityType TEXT,
     RelatedEntityId BIGINT,
-    RelatedEntityType VARCHAR(2),
+    RelatedEntityType TEXT,
     RelatedType SMALLINT,
     Score FLOAT8
   );
 
-\COPY mag.EntityRelatedEntities(EntityId, EntityType, RelatedEntityId, RelatedEntityType, RelatedType, Score) FROM '../input/export/advanced/EntityRelatedEntities.txt' null as '';
+\COPY mag.EntityRelatedEntities(EntityId, EntityType, RelatedEntityId, RelatedEntityType, RelatedType, Score) FROM PROGRAM 'tail -n+2 input/advanced/EntityRelatedEntities.txt' null as '';
 
 CREATE INDEX idx_EntityRelatedEntities_EntityId ON mag.EntityRelatedEntities(EntityId);
 CREATE INDEX idx_EntityRelatedEntities_RelatedEntityId ON mag.EntityRelatedEntities(RelatedEntityId);
@@ -399,7 +397,7 @@ CREATE TABLE mag.PaperRecommendations(
     Score FLOAT8
   );
 
-\COPY mag.PaperRecommendations(PaperId, RecommendedPaperId, Score) FROM '../input/export/advanced/PaperRecommendations.txt' null as '';
+\COPY mag.PaperRecommendations(PaperId, RecommendedPaperId, Score) FROM PROGRAM 'tail -n+2 input/advanced/PaperRecommendations.txt' null as '';
 
 CREATE INDEX idx_PaperRecommendations_PaperId ON mag.PaperRecommendations(PaperId);
 CREATE INDEX idx_PaperRecommendations_RecommendedPaperId ON mag.PaperRecommendations(RecommendedPaperId);
@@ -416,7 +414,7 @@ CREATE TABLE mag.PaperFieldsOfStudy(
     AlgorithmVersion INT
   );
 
-\COPY mag.PaperFieldsOfStudy(PaperId, FieldOfStudyId, Score, AlgorithmVersion) FROM '../input/export/advanced/PaperFieldsOfStudy.txt' null as '';
+\COPY mag.PaperFieldsOfStudy(PaperId, FieldOfStudyId, Score, AlgorithmVersion) FROM PROGRAM 'tail -n+2 input/advanced/PaperFieldsOfStudy.txt' null as '';
 
 CREATE INDEX idx_PaperFieldsOfStudy_PaperId ON mag.PaperFieldsOfStudy(PaperId);
 CREATE INDEX idx_PaperFieldsOfStudy_FieldOfStudyId ON mag.PaperFieldsOfStudy(FieldOfStudyId);
@@ -431,7 +429,7 @@ CREATE TABLE mag.FieldOfStudyChildren(
     ChildFieldOfStudyId BIGINT
   );
 
-\COPY mag.FieldOfStudyChildren(FieldOfStudyId, ChildFieldOfStudyId) FROM '../input/export/advanced/FieldOfStudyChildren.txt' null as '';
+\COPY mag.FieldOfStudyChildren(FieldOfStudyId, ChildFieldOfStudyId) FROM PROGRAM 'tail -n+2 input/advanced/FieldOfStudyChildren.txt' null as '';
 
 CREATE INDEX idx_FieldOfStudyChildren_FieldOfStudyId ON mag.FieldOfStudyChildren(FieldOfStudyId);
 CREATE INDEX idx_FieldOfStudyChildren_ChildFieldOfStudyId ON mag.FieldOfStudyChildren(ChildFieldOfStudyId);
@@ -447,7 +445,7 @@ CREATE TABLE mag.FieldOfStudyExtendedAttributes(
     AttributeValue TEXT
   );
 
-\COPY mag.FieldOfStudyExtendedAttributes(FieldOfStudyId, AttributeType, AttributeValue) FROM '../input/export/advanced/FieldOfStudyExtendedAttributes.txt' null as '';
+\COPY mag.FieldOfStudyExtendedAttributes(FieldOfStudyId, AttributeType, AttributeValue) FROM program 'tail -n+2 input/advanced/FieldOfStudyExtendedAttributes.txt' null as '';
 
 CREATE INDEX idx_FieldOfStudyExtendedAttributes_FieldOfStudyId ON mag.FieldOfStudyExtendedAttributes(FieldOfStudyId);
 
@@ -458,9 +456,9 @@ DROP TABLE IF EXISTS mag.FieldsOfStudy;
 CREATE TABLE mag.FieldsOfStudy(
     FieldOfStudyId BIGINT PRIMARY KEY,
     Rank INT,
-    NormalizedName VARCHAR(255),
-    DisplayName VARCHAR(255),
-    MainType VARCHAR(255),
+    NormalizedName TEXT,
+    DisplayName TEXT,
+    MainType TEXT,
     Level SMALLINT,
     PaperCount BIGINT,
     PaperFamilyCount BIGINT,
@@ -468,7 +466,7 @@ CREATE TABLE mag.FieldsOfStudy(
     CreatedDate DATE
   );
 
-\COPY mag.FieldsOfStudy(FieldOfStudyId, Rank, NormalizedName, DisplayName, MainType, Level, PaperCount, PaperFamilyCount, CitationCount, CreatedDate) FROM '../input/export/advanced/FieldsOfStudy.txt' null as '';
+\COPY mag.FieldsOfStudy(FieldOfStudyId, Rank, NormalizedName, DisplayName, MainType, Level, PaperCount, PaperFamilyCount, CitationCount, CreatedDate) FROM PROGRAM 'tail -n+2 input/advanced/FieldsOfStudy.txt' null as '';
 
 --------------------------------------------------------
 -- RelatedFieldOfStudy
@@ -477,13 +475,13 @@ DROP TABLE IF EXISTS mag.RelatedFieldOfStudy;
 CREATE TABLE mag.RelatedFieldOfStudy(
     id SERIAL PRIMARY KEY,
     FieldOfStudyId1 BIGINT,
-    Type1 VARCHAR(255),
+    Type1 TEXT,
     FieldOfStudyId2 BIGINT,
-    Type2 VARCHAR(255),
+    Type2 TEXT,
     Rank FLOAT8
   );
 
-\COPY mag.RelatedFieldOfStudy(FieldOfStudyId1, Type1, FieldOfStudyId2, Type2, Rank) FROM '../input/export/advanced/RelatedFieldOfStudy.txt' null as '';
+\COPY mag.RelatedFieldOfStudy(FieldOfStudyId1, Type1, FieldOfStudyId2, Type2, Rank) FROM PROGRAM 'tail -n+2 input/advanced/RelatedFieldOfStudy.txt' null as '';
 
 CREATE INDEX idx_RelatedFieldOfStudy_FieldOfStudyId1 ON mag.RelatedFieldOfStudy(FieldOfStudyId1);
 CREATE INDEX idx_RelatedFieldOfStudy_FieldOfStudyId2 ON mag.RelatedFieldOfStudy(FieldOfStudyId2);
@@ -502,7 +500,7 @@ CREATE TABLE mag.PaperMeSH(
     IsMajorTopic BOOLEAN
   );
 
-\COPY mag.PaperMeSH(PaperId, DescriptorUI, DescriptorName, QualifierUI, QualifierName, IsMajorTopic) FROM '../input/export/advanced/PaperMeSH.txt' null as '';
+\COPY mag.PaperMeSH(PaperId, DescriptorUI, DescriptorName, QualifierUI, QualifierName, IsMajorTopic) FROM PROGRAM 'tail -n+2 input/advanced/PaperMeSH.txt' null as '';
 
 CREATE INDEX idx_PaperMeSH_PaperId ON mag.PaperMeSH(PaperId);
 CREATE INDEX idx_PaperMeSH_DescriptorUI ON mag.PaperMeSH(DescriptorUI);
